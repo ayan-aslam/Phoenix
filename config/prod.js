64551efb3204124bdf1c18d3558b41070754019e
ayan-aslam <ayaninstudy10000@gmail.com>
2025-06-04 02:27:26 +0530

    Authentication Attempt 2 - Fixed String Error

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 // Production keys will be placed here.
 
 module.exports = {
-    googleClientID: 'process.env.GOOGLE_CLIENT_ID',
-    googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
-    mongoURI: 'process.env.MONGO_URI',
-    cookieKey: 'process.env.COOKIE_KEY'
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY
 }
